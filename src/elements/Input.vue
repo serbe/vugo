e31257dbd337b182f82a2dca55c3677ec205ed51
Valeriy Kostikov <mkoctuk@gmail.com>
2017-04-13 16:04:35 +0300

    remove duplicate code, add blur

--- src/elements/Input.vue
+++ src/elements/Input.vue
@@ -3,64 +3,29 @@
     <label v-if="getLabel" class="label">{{ getLabel }}</label>
     <p :class="classList">
       <a v-if="hyper" :href="hyper">
-        <input v-if="type=='text'"
+        <input
           :class="inputClassList"
-          type="text"
+          :type="type"
           ref="input"
           :name="name"
           :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
-          :autocomplete="autocomplete">
-        <input v-if="type=='email'"
-          :class="inputClassList"
-          type="email"
-          ref="input"
-          :name="name"
-          :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
-          :autocomplete="autocomplete">
-        <input v-if="type=='tel'"
-          :class="inputClassList"
-          type="tel"
-          ref="input"
-          :name="name"
-          :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
+          :value="value"
+          @input="onInput"
           :autocomplete="autocomplete">
         <span v-if="icon" class="icon is-small">
           <i :class="'fa fa-' + icon"></i>
         </span>
       </a>
       <template v-else>
-        <input v-if="type=='text'"
+        <input
           :class="inputClassList"
-          type="text"
+          :type="type"
           ref="input"
           :name="name"
           :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
-          :autocomplete="autocomplete">
-        <input v-if="type=='email'"
-          :class="inputClassList"
-          type="email"
-          ref="input"
-          :name="name"
-          :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
-          :autocomplete="autocomplete">
-        <input v-if="type=='tel'"
-          :class="inputClassList"
-          type="tel"
-          ref="input"
-          :name="name"
-          :placeholder="placeholder"
-          v-bind:value="value"
-          v-on:input="updateValue($event.target.value)"
+          :value="value"
+          @input="onInput"
+          @blur="onBlur"
           :autocomplete="autocomplete">
         <span v-if="icon" class="icon is-small">
           <i :class="'fa fa-' + icon"></i>
@@ -73,6 +38,50 @@
 <script>
   export default {
     name: 'vue-input',
+    data () {
+      return {
+        inputValue: this.value
+      }
+    },
+    computed: {
+      classList () {
+        var res = ['control is-expanded']
+        if (this.icon) {
+          res.push('has-icon')
+        }
+        return res
+      },
+      inputClassList () {
+        var res = ['input']
+        if (this.color) {
+          res.push(this.color.split(' ').map(e => { return 'is-' + e }))
+        }
+        if (this.size) {
+          res.push(this.size.split(' ').map(e => { return 'is-' + e }))
+        }
+        if (this.state) {
+          res.push(this.state.split(' ').map(e => { return 'is-' + e }))
+        }
+        return res
+      },
+      getLabel () {
+        if (this.label !== false && this.placeholder !== false && this.label === '') {
+          return this.placeholder
+        } else {
+          return this.label
+        }
+      }
+    },
+    methods: {
+      onInput (event) {
+        const val = event.target.value
+        this.newValue = val
+        this.$emit('input', val)
+      },
+      onBlur (event) {
+        this.$emit('blur', event)
+      }
+    },
     props: {
       value: '',
       type: {
@@ -123,44 +132,6 @@
         type: [String, Boolean],
         required: false,
         default: false
-      }
-    },
-    computed: {
-      classList () {
-        var res = ['control is-expanded']
-        if (this.icon) {
-          res.push('has-icon')
-        }
-        return res
-      },
-      inputClassList () {
-        var res = ['input']
-        if (this.color) {
-          res.push(this.color.split(' ').map(e => { return 'is-' + e }))
-        }
-        if (this.size) {
-          res.push(this.size.split(' ').map(e => { return 'is-' + e }))
-        }
-        if (this.state) {
-          res.push(this.state.split(' ').map(e => { return 'is-' + e }))
-        }
-        return res
-      },
-      getLabel () {
-        if (this.label !== false && this.placeholder !== false && this.label === '') {
-          return this.placeholder
-        } else {
-          return this.label
-        }
-      }
-    },
-    methods: {
-      updateValue: function (value) {
-        var formattedValue = value.trim()
-        if (formattedValue !== value) {
-          this.$refs.input.value = formattedValue
-        }
-        this.$emit('input', formattedValue)
       }
     }
   }
