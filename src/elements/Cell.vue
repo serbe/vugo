737a248711a7c0fe6dcbc7f967cb038b732844d3
Serb <mkoctuk@gmail.com>
2017-05-11 11:14:37 +0300

    add cell

---
+++ src/elements/Cell.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <template v-if="type === 'text'">{{ value }}</template>
+    <template v-else-if="type === 'array'"><span v-for="item in value">{{ item }}<br></span></template>
+    <template v-else-if="type === 'checkbox'"><input type="checkbox" disabled :checked="value"></template>
+    <template v-else>{{ value }}</template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vue-cell',
+    data () {
+      return {
+        // query: '',
+        // page: 1,
+        // list: []
+      }
+    },
+    props: {
+      type: {
+        type: String,
+        required: false,
+        default: 'text'
+      },
+      value: {
+        required: false
+      }
+    }
+  }
+</script>
+
