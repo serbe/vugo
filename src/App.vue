95f81478d8ae63ed69eb1848e60f0c53480b95cb
Valeriy Kostikov <mkoctuk@gmail.com>
2017-06-09 19:13:16 +0300

    fmt

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-      <vue-navigation/>
-      <section class="section pt1">
+    <vue-navigation/>
+    <section class="section pt1">
       <div class="container">
         <!--<img src="./assets/logo.png">-->
         <router-view></router-view>
       </div>
-      </section>
+    </section>
   </div>
 </template>
 
@@ -20,32 +20,32 @@ export default {
 </script>
 
 <style lang="scss">
-  body {
-    font-size: 0.85rem !important;
-  }
+body {
+  font-size: 0.85rem !important;
+}
 
-  .select select {
-    padding-right: 2.5em !important;
-  }
+.select select {
+  padding-right: 2.5em !important;
+}
 
-  .pt1 {
-    padding-top: 1rem !important;
-  }
+.pt1 {
+  padding-top: 1rem !important;
+}
 
-  $primary: #2B2B2B;
+$primary: #2B2B2B;
 
-  $text: hsl(0, 0%, 14%);
+$text: hsl(0, 0%, 14%);
 
-  $size-1:	2.85rem;
-  $size-2:	2.35rem;
-  $size-3:	1.85rem;
-  $size-4:	1.35rem;
-  $size-5:	1.1rem;
-  $size-6:	0.85rem;
-  $size-7:	0.6rem;
+$size-1: 2.85rem;
+$size-2: 2.35rem;
+$size-3: 1.85rem;
+$size-4: 1.35rem;
+$size-5: 1.1rem;
+$size-6: 0.85rem;
+$size-7: 0.6rem;
 
-  @import "~bulma/bulma.sass";
+@import "~bulma/bulma.sass";
 
-  $fa-font-path: "~font-awesome/fonts";
-  @import "~font-awesome/scss/font-awesome";
+$fa-font-path: "~font-awesome/fonts";
+@import "~font-awesome/scss/font-awesome";
 </style>
