867a2cb1ddc1d2e5ba8b867b942debe34707b5a7
Valeriy Kostikov <mkoctuk@gmail.com>
2017-06-27 18:46:20 +0300

    add near practices

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,76 @@
 <template>
   <div class="content has-text-centered">
-    <h2>{{ dwidth }}</h2>
-    <h2>{{ dpr }}</h2>
+    <div class="columns">
+      <div class="column is-one-third">
+      </div>
+      <div class="column">
+        <table class="table is-striped">
+          <thead>
+            <tr>
+              <th><a>Дата тренировки</a></th>
+              <th><a>Тип тренировки</a></th>
+              <th><a>Организация</a></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(item, index) in practicesList" v-bind:key="index">
+              <td><a :href="'/practice/' + item.id">{{ item.date_str }}</a></td>
+              <td><a :href="'/practice/' + item.id">{{ item.kind_name }}</a></td>
+              <td><a :href="'/company/' + item.company_id">{{ item.company_name }}</a></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import vtable from '@/elements/Table'
+import request from '@/request'
+
 export default {
   name: 'home',
+  components: {
+    'vue-table': vtable
+  },
   data () {
     return {
-
+      fetched: false,
+      practicesList: []
     }
   },
   computed: {
-    dwidth () {
-      return (window.innerWidth > 0) ? window.innerWidth : screen.width
+    auth () {
+      return this.$store.getters.isAuth
+    }
+  },
+  created () {
+    this.fetchPractices()
+  },
+  methods: {
+    fetchPractices () {
+      if (this.auth && !this.fetched) {
+        request({
+          url: 'practices/near',
+          method: 'GET'
+        })
+        .then(r => {
+          this.practicesList = this.createList(r.data['practices'])
+          this.fetched = true
+        })
+      }
     },
-    dpr () {
-      return window.devicePixelRatio
+    createList (practices) {
+      let practicesList = []
+      if (practices) {
+        practicesList = practices.map(e => {
+          let str = [e.date_str, e.kind_name, e.note]
+          e.str = str.join(' ').toLowerCase()
+          return e
+        })
+      }
+      return practicesList
     }
   }
 }
