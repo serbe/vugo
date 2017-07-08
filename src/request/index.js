d3ed7c03a827d231468c78db445408bd08e815b1
Valeriy Kostikov <mkoctuk@gmail.com>
2017-07-08 23:23:32 +0300

    fix eslint func-names

--- src/request/index.js
+++ src/request/index.js
@@ -12,36 +12,36 @@ const client = axios.create({
   baseURL,
 });
 
-const request = function (options) {
+function request(options) {
   if (auth.isAuth()) {
     client.defaults.headers.common.Authorization = auth.getToken();
   }
 
-  const onSuccess = function (response) {
+  function onSuccess(response) {
     // console.debug('Request Successful!', response)
     // return response.data
     return response;
-  };
+  }
 
-  const onError = function (error) {
-    console.error('Request Failed:', error.config);
+  function onError(error) {
+    // console.error('Request Failed:', error.config);
     if (error.response) {
       if (error.response.status === 401 && auth.isAuth()) {
         auth.logout();
         router.push({ name: 'login' });
       }
-      console.error('Status:', error.response.status);
-      console.error('Data:', error.response.data);
-      console.error('Headers:', error.response.headers);
+      // console.error('Status:', error.response.status);
+      // console.error('Data:', error.response.data);
+      // console.error('Headers:', error.response.headers);
     } else {
-      console.error('Error Message:', error.message);
+      // console.error('Error Message:', error.message);
     }
     return Promise.reject(error.response || error.message);
-  };
+  }
 
   return client(options)
     .then(onSuccess)
     .catch(onError);
-};
+}
 
 export default request;
