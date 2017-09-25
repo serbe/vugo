61877b0d74fdc69eb61e7490c93630224d499724
Valeriy Kostikov <mkoctuk@gmail.com>
2017-09-25 19:23:21 +0300

    update

--- build/dev-server.js
+++ build/dev-server.js
@@ -29,8 +29,9 @@ var devMiddleware = require('webpack-dev-middleware')(compiler, {
 });
 
 var hotMiddleware = require('webpack-hot-middleware')(compiler, {
-  log: () => {}
-});
+  log: false,
+  heartbeat: 2000
+})
 // force page reload when html-webpack-plugin template changes
 compiler.plugin('compilation', function (compilation) {
   compilation.plugin('html-webpack-plugin-after-emit', function (data, cb) {
