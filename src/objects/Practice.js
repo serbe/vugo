08c80ba49bbcd141ff2db5a17d03cbd881bad535
Serbe <mkoctuk@gmail.com>
2019-08-07 13:26:15 +0300

    remove unused fields

--- src/objects/Practice.js
+++ src/objects/Practice.js
@@ -4,10 +4,8 @@ export default {
   id: 0,
   company_id: 0,
   company: SelectItem,
-  company_name: "",
   kind_id: 0,
   kind: SelectItem,
-  kind_name: "",
   date_of_practice: "",
   topic: "",
   note: ""
